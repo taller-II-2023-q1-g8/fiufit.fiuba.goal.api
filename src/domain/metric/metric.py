3acd19af6290bf49ac105bbd2e51db1921f78c62
F-Papa <francopapa21@gmail.com>
2023-04-28 12:49:52 -0300

    metric dataclasses

---
+++ src/domain/metric/metric.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class Metric:
+    crated_at: datetime
+    username: str
+
+
+@dataclass
+class TrainingPlanCompleted(Metric):
+    """A metric that represents the completion of a training plan."""
+    plan_title: str
+
+
+@dataclass
+class ExcerciseSetCompleted(Metric):
+    """A metric that represents the completion of an excercise"""
+    excercise_title: str
+    weight: float
+    reps: int
+
+
+@dataclass
+class WeightMeasured(Metric):
+    """A metric that represents the user's weight measured"""
+    weight: float
+
+
+@dataclass
+class DistanceTravelled(Metric):
+    """A metric that represents a walk."""
+    distance: float
+    duration: float
+
+    def pace(self):
+        return self.distance / self.duration
+
+
+@dataclass
+class CaloriesBurned(Metric):
+    """A metric that represents the calories burned by the user"""
+    calories: float
